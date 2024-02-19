ec5595e4fa6b4bd726de20c2576e452f73f19fa5
Uwe L. Korn <uwe.korn@quantco.com>
2024-02-19 16:55:42 +0100

    Force pkg-config from build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 set -exuo pipefail
 
-VALAC=/no-valac ./configure --prefix=${PREFIX} || (cat config.log; exit 1)
+PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config VALAC=/no-valac ./configure --prefix=${PREFIX} || (cat config.log; exit 1)
 make -j${CPU_COUNT}
 make install
 
